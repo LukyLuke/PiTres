99ad8a1e6f64e1d0866f5ff6c5dc2e1ec92c0d13
LukyLuke <lukas@delightsoftware.com>
2012-04-15 10:47:00 +0200

    Added and implemented PPSPerson

---
+++ src/data/Person.cpp
@@ -0,0 +1,332 @@
+
+#include "Person.h"
+
+#include <QObject>
+#include <QString>
+#include <QVariant>
+#include <QDate>
+#include <QList>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
+
+PPSPerson::PPSPerson(QObject *parent) {
+	clear();
+}
+
+PPSPerson::~PPSPerson() {}
+
+void PPSPerson::createTables(QSqlDatabase db) {
+	QSqlQuery query(db);
+	query.prepare("CREATE TABLE IF NOT EXISTS ldap_persons (uid INTEGER, contribution INTEGER, nickname TEXT, gender TEXT, familyname TEXT, "
+	              "givenname TEXT, address TEXT, plz TEXT, city TEXT, country TEXT, state TEXT, birthday DATE, language TEXT, joining DATE, section TEXT);");
+	query.exec();
+	if (query.lastError().type() != QSqlError::NoError) {
+		qDebug() << query.lastQuery();
+		qDebug() << query.lastError();
+	}
+	
+	query.prepare("CREATE TABLE IF NOT EXISTS ldap_persons_email (uid INTEGER, type TEXT, mail TEXT);");
+	query.exec();
+	if (query.lastError().type() != QSqlError::NoError) {
+		qDebug() << query.lastQuery();
+		qDebug() << query.lastError();
+	}
+	
+	query.prepare("CREATE TABLE IF NOT EXISTS ldap_persons_telephone (uid INTEGER, type TEXT, number TEXT);");
+	query.exec();
+	if (query.lastError().type() != QSqlError::NoError) {
+		qDebug() << query.lastQuery();
+		qDebug() << query.lastError();
+	}
+	
+	query.prepare("CREATE TABLE IF NOT EXISTS ldap_persons_mobile (uid INTEGER, type TEXT, number TEXT);");
+	query.exec();
+	if (query.lastError().type() != QSqlError::NoError) {
+		qDebug() << query.lastQuery();
+		qDebug() << query.lastError();
+	}
+}
+
+void PPSPerson::emptyTables(QSqlDatabase db) {
+	QSqlQuery query(db);
+	query.prepare("DELETE FROM ldap_persons;");
+	query.exec();
+	if (query.lastError().type() != QSqlError::NoError) {
+		qDebug() << query.lastQuery();
+		qDebug() << query.lastError();
+	}
+	
+	query.prepare("DELETE FROM ldap_persons_email;");
+	query.exec();
+	if (query.lastError().type() != QSqlError::NoError) {
+		qDebug() << query.lastQuery();
+		qDebug() << query.lastError();
+	}
+	
+	query.prepare("DELETE FROM ldap_persons_telephone;");
+	query.exec();
+	if (query.lastError().type() != QSqlError::NoError) {
+		qDebug() << query.lastQuery();
+		qDebug() << query.lastError();
+	}
+	
+	query.prepare("DELETE FROM ldap_persons_mobile;");
+	query.exec();
+	if (query.lastError().type() != QSqlError::NoError) {
+		qDebug() << query.lastQuery();
+		qDebug() << query.lastError();
+	}
+	
+	query.prepare("VACUUM;");
+	query.exec();
+}
+
+void PPSPerson::save(QSqlDatabase db) {
+	int i;
+	QSqlQuery query(db);
+	query.prepare("INSERT INTO ldap_persons (uid, contribution, nickname, gender, familyname, givenname, address, plz, city, country, state, birthday, language, joining, section) "
+	              "VALUES (:uid, :contcls, :nickname, :gender, :familyname, :givenname, :address, :plz, :city, :country, :state, :birthday, :language, :joining, :section);");
+	query.bindValue(":uid", s_uid);
+	query.bindValue(":section", s_section);
+	query.bindValue(":contcls", (int)m_contributionClass);
+	query.bindValue(":nickname", s_nickname);
+	query.bindValue(":gender", (int)m_gender);
+	query.bindValue(":familyname", s_familyName);
+	query.bindValue(":givenname", s_givenName);
+	query.bindValue(":address", s_street);
+	query.bindValue(":plz", s_postalCode);
+	query.bindValue(":city", s_city);
+	query.bindValue(":country", s_country);
+	query.bindValue(":state", s_state);
+	query.bindValue(":birthday", d_birthdate);
+	query.bindValue(":joining", d_joining);
+	query.bindValue(":language", (int)m_language);
+	query.exec();
+	
+	if (query.lastError().type() != QSqlError::NoError) {
+		qDebug() << query.lastQuery();
+		qDebug() << query.lastError();
+	}
+	
+	// Insert all telephones, mobiles and email addresses
+	if (!l_telephone.empty()) {
+		query.prepare("INSERT INTO ldap_persons_telephone (uid, type, number) VALUES (:uid, :type, :value);");
+		query.bindValue(":uid", s_uid);
+		query.bindValue(":type", "default");
+		for (i = 0; i < l_telephone.size(); i++) {
+			query.bindValue(":value", l_telephone.at(i));
+			query.exec();
+			if (query.lastError().type() != QSqlError::NoError) {
+				qDebug() << query.lastQuery();
+				qDebug() << query.lastError();
+			}
+		}
+	}
+	
+	if (!l_mobile.empty()) {
+		query.prepare("INSERT INTO ldap_persons_mobile (uid, type, number) VALUES (:uid, :type, :value);");
+		query.bindValue(":uid", s_uid);
+		query.bindValue(":type", "default");
+		for (i = 0; i < l_mobile.size(); i++) {
+			query.bindValue(":value", l_mobile.at(i));
+			query.exec();
+			if (query.lastError().type() != QSqlError::NoError) {
+				qDebug() << query.lastQuery();
+				qDebug() << query.lastError();
+			}
+		}
+	}
+	
+	if (!l_email.empty()) {
+		query.prepare("INSERT INTO ldap_persons_email (uid, type, mail) VALUES (:uid, :type, :value);");
+		query.bindValue(":uid", s_uid);
+		query.bindValue(":type", "default");
+		for (i = 0; i < l_email.size(); i++) {
+			query.bindValue(":value", l_email.at(i));
+			query.exec();
+			if (query.lastError().type() != QSqlError::NoError) {
+				qDebug() << query.lastQuery();
+				qDebug() << query.lastError();
+			}
+		}
+	}
+}
+
+void PPSPerson::clear() {
+	s_uid = "";
+	s_section = "";
+	m_contributionClass = Full;
+	s_nickname = "";
+	m_gender = Unknown;
+	s_familyName = "";
+	s_givenName = "";
+	s_street = "";
+	s_postalCode = "";
+	s_city = "";
+	s_country = "";
+	s_state = "";
+	l_telephone.clear();
+	l_mobile.clear();
+	l_email.clear();
+	d_birthdate = QDate(1970, 01, 01);
+	d_joining = QDate::currentDate();
+	m_language = EN;
+}
+
+void PPSPerson::setUid(QString uid) {
+	s_uid = uid;
+	emit uidChanged(uid);
+}
+
+void PPSPerson::setSection(QString section) {
+	s_section = section;
+	emit sectionChanged(section);
+}
+
+void PPSPerson::setContributionClass(ContributionClass contributionClass) {
+	m_contributionClass = contributionClass;
+	emit contributionClassChanged(contributionClass);
+}
+
+void PPSPerson::setNickname(QString nickname) {
+	s_nickname = nickname;
+	emit nicknameChanged(nickname);
+}
+
+void PPSPerson::setGender(Gender gender) {
+	m_gender = gender;
+	emit genderChanged(gender);
+}
+
+void PPSPerson::setFamilyName(QString familyName) {
+	s_familyName = familyName;
+	emit familyNameChanged(familyName);
+}
+
+void PPSPerson::setGivenName(QString givenName) {
+	s_givenName = givenName;
+	emit givenNameChanged(givenName);
+}
+
+void PPSPerson::setStreet(QString street) {
+	s_street = street;
+	emit streetChanged(street);
+}
+
+void PPSPerson::setPostalCode(QString postalCode) {
+	s_postalCode = postalCode;
+	emit postalCodeChanged(postalCode);
+}
+
+void PPSPerson::setCity(QString city) {
+	s_city = city;
+	emit cityChanged(city);
+}
+
+void PPSPerson::setCountry(QString country) {
+	s_country = country;
+	emit countryChanged(country);
+}
+
+void PPSPerson::setState(QString state) {
+	s_state = state;
+	emit stateChanged(state);
+}
+
+void PPSPerson::addTelephone(QString number) {
+	l_telephone.append(number);
+	emit telephoneAdded(number);
+}
+
+void PPSPerson::removeTelephone(QString number) {
+	int pos = l_telephone.indexOf(number);
+	if (pos >= 0 && pos < l_telephone.size()) {
+		l_telephone.removeAt(pos);
+		emit telephoneRemoved(number);
+	}
+}
+
+void PPSPerson::updateTelephone(QString oldNumber, QString newNumber) {
+	int pos = l_telephone.indexOf(oldNumber);
+	if (pos >= 0 && pos < l_telephone.size()) {
+		l_telephone.replace(pos, newNumber);
+		emit telephoneUpdated(oldNumber, newNumber);
+	}
+}
+
+void PPSPerson::setTelephone(QList<QString> numbers) {
+	l_telephone.clear();
+	l_telephone.append(numbers);
+	emit telephoneChanged(l_telephone);
+}
+
+void PPSPerson::addMobile(QString number) {
+	l_mobile.append(number);
+	emit mobileAdded(number);
+}
+
+void PPSPerson::removeMobile(QString number) {
+	int pos = l_mobile.indexOf(number);
+	if (pos >= 0 && pos < l_mobile.size()) {
+		l_mobile.removeAt(pos);
+		emit mobileRemoved(number);
+	}
+}
+
+void PPSPerson::updateMobile(QString oldNumber, QString newNumber) {
+	int pos = l_mobile.indexOf(oldNumber);
+	if (pos >= 0 && pos < l_mobile.size()) {
+		l_mobile.replace(pos, newNumber);
+		emit mobileUpdated(oldNumber, newNumber);
+	}
+}
+
+void PPSPerson::setMobile(QList<QString> numbers) {
+	l_mobile.clear();
+	l_mobile.append(numbers);
+	emit mobileChanged(l_mobile);
+}
+
+void PPSPerson::addEmail(QString address) {
+	l_email.append(address);
+	emit emailAdded(address);
+}
+
+void PPSPerson::removeEmail(QString address) {
+	int pos = l_email.indexOf(address);
+	if (pos >= 0 && pos < l_email.size()) {
+		l_email.removeAt(pos);
+		emit emailRemoved(address);
+	}
+}
+
+void PPSPerson::updateEmail(QString oldAddress, QString newAddress) {
+	int pos = l_email.indexOf(oldAddress);
+	if (pos >= 0 && pos < l_email.size()) {
+		l_email.replace(pos, newAddress);
+		emit emailUpdated(oldAddress, newAddress);
+	}
+}
+
+void PPSPerson::setEmail(QList<QString> addresses) {
+	l_email.clear();
+	l_email.append(addresses);
+	emit emailChanged(l_email);
+}
+
+void PPSPerson::setBirthdate(QDate birthdate) {
+	d_birthdate = birthdate;
+	emit birthdateChanged(birthdate);
+}
+
+void PPSPerson::setJoining(QDate joining) {
+	d_joining = joining;
+	emit joiningChanged(joining);
+}
+
+void PPSPerson::setLanguage(Language language) {
+	m_language = language;
+	emit languageChanged(language);
+}
---
+++ src/data/Person.h
@@ -0,0 +1,146 @@
+#ifndef Data_Person_H
+#define Data_Person_H
+
+#include <QObject>
+#include <QString>
+#include <QDate>
+#include <QList>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+
+class PPSPerson : public QObject {
+Q_OBJECT
+Q_PROPERTY(QString uid READ uid WRITE setUid NOTIFY uidChanged)
+Q_PROPERTY(QString section READ section WRITE setSection NOTIFY sectionChanged)
+Q_PROPERTY(ContributionClass contributionClass READ contributionClass WRITE setContributionClass NOTIFY contributionClassChanged)
+Q_PROPERTY(QString nickname READ nickname WRITE setNickname NOTIFY nicknameChanged)
+Q_PROPERTY(Gender gender READ gender WRITE setGender NOTIFY genderChanged)
+Q_PROPERTY(QString familyName READ familyName WRITE setFamilyName NOTIFY familyNameChanged)
+Q_PROPERTY(QString givenName READ givenName WRITE setGivenName NOTIFY givenNameChanged)
+Q_PROPERTY(QString street READ street WRITE setStreet NOTIFY streetChanged)
+Q_PROPERTY(QString postalCode READ postalCode WRITE setPostalCode NOTIFY postalCodeChanged)
+Q_PROPERTY(QString city READ city WRITE setCity NOTIFY cityChanged)
+Q_PROPERTY(QString country READ country WRITE setCountry NOTIFY countryChanged)
+Q_PROPERTY(QString state READ state WRITE setState NOTIFY stateChanged)
+Q_PROPERTY(QList<QString> telephone READ telephone WRITE setTelephone NOTIFY telephoneChanged)
+Q_PROPERTY(QList<QString> mobile READ mobile WRITE setMobile NOTIFY mobileChanged)
+Q_PROPERTY(QList<QString> email READ email WRITE setEmail NOTIFY emailChanged)
+Q_PROPERTY(QDate birthdate READ birthdate WRITE setBirthdate NOTIFY birthdateChanged)
+Q_PROPERTY(QDate joining READ joining WRITE setJoining NOTIFY joiningChanged)
+Q_PROPERTY(Language language READ language WRITE setLanguage NOTIFY languageChanged)
+Q_ENUMS(ContributionClass)
+Q_ENUMS(Gender)
+Q_ENUMS(Language)
+
+public:
+	PPSPerson(QObject *parent = 0);
+	virtual ~PPSPerson();
+	void save(QSqlDatabase db);
+	void clear();
+	static void createTables(QSqlDatabase db);
+	static void emptyTables(QSqlDatabase db);
+	
+	enum ContributionClass { Full=0, Student=1 };
+	enum Gender { Male=0, Female=1, Both=2, None=3, Unknown=-1 };
+	enum Language { DE=0, FR=1, IT=2, EN=3 };
+	
+	// Setter
+	void setUid(QString uid);
+	void setSection(QString section);
+	void setContributionClass(ContributionClass contributionClass);
+	void setNickname(QString nickname);
+	void setGender(Gender gender);
+	void setFamilyName(QString familyName);
+	void setGivenName(QString givenName);
+	void setStreet(QString street);
+	void setPostalCode(QString postalCode);
+	void setCity(QString city);
+	void setCountry(QString country);
+	void setState(QString state);
+	void addTelephone(QString number);
+	void removeTelephone(QString number);
+	void updateTelephone(QString oldNumber, QString newNumber);
+	void setTelephone(QList<QString> numbers);
+	void addMobile(QString number);
+	void removeMobile(QString number);
+	void updateMobile(QString oldNumber, QString newNumber);
+	void setMobile(QList<QString> numbers);
+	void addEmail(QString address);
+	void removeEmail(QString address);
+	void updateEmail(QString oldAddress, QString newAddress);
+	void setEmail(QList<QString> addresses);
+	void setBirthdate(QDate birthdate);
+	void setJoining(QDate joining);
+	void setLanguage(Language language);
+	
+	// Getter
+	QString uid() const { return s_uid; };
+	QString section() const { return s_section; };
+	ContributionClass contributionClass() const { return m_contributionClass; };
+	QString nickname() const { return s_nickname; };
+	Gender gender() const { return m_gender; };
+	QString familyName() const { return s_familyName; };
+	QString givenName() const { return s_givenName; };
+	QString street() const { return s_street; };
+	QString postalCode() const { return s_postalCode; };
+	QString city() const { return s_city; };
+	QString country() const { return s_country; };
+	QString state() const { return s_state; };
+	QList<QString> telephone() const { return l_telephone; };
+	QList<QString> mobile() const { return l_mobile; };
+	QList<QString> email() const { return l_email; };
+	QDate birthdate() const { return d_birthdate; };
+	QDate joining() const { return d_joining; };
+	Language language() const { return m_language; };
+
+signals:
+	void uidChanged(QString);
+	void sectionChanged(QString);
+	void contributionClassChanged(ContributionClass);
+	void nicknameChanged(QString);
+	void genderChanged(Gender);
+	void familyNameChanged(QString);
+	void givenNameChanged(QString);
+	void streetChanged(QString);
+	void postalCodeChanged(QString);
+	void cityChanged(QString);
+	void countryChanged(QString);
+	void stateChanged(QString);
+	void telephoneAdded(QString);
+	void telephoneRemoved(QString);
+	void telephoneUpdated(QString, QString);
+	void telephoneChanged(QList<QString>);
+	void mobileAdded(QString);
+	void mobileRemoved(QString);
+	void mobileUpdated(QString, QString);
+	void mobileChanged(QList<QString>);
+	void emailAdded(QString);
+	void emailRemoved(QString);
+	void emailUpdated(QString, QString);
+	void emailChanged(QList<QString>);
+	void birthdateChanged(QDate);
+	void joiningChanged(QDate);
+	void languageChanged(Language);
+
+private:
+	QString s_uid;
+	QString s_section;
+	ContributionClass m_contributionClass;
+	QString s_nickname;
+	Gender m_gender;
+	QString s_familyName;
+	QString s_givenName;
+	QString s_street;
+	QString s_postalCode;
+	QString s_city;
+	QString s_country;
+	QString s_state;
+	QList<QString> l_telephone;
+	QList<QString> l_mobile;
+	QList<QString> l_email;
+	QDate d_birthdate;
+	QDate d_joining;
+	Language m_language;
+};
+
+#endif // Data_Person_H
