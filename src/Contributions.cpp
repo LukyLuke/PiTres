a3dbbaacec15c6b2ed70be8261a2a665c085ed3b
LukyLuke <lukas@delightsoftware.com>
2012-08-01 18:02:48 +0200

    Added individual Memofield and changed some functionality

--- src/Contributions.cpp
+++ src/Contributions.cpp
@@ -10,6 +10,7 @@
 #include <cstdio>
 #include <QFileDialog>
 #include <QFile>
+#include <QList>
 #include <QSettings>
 
 Contributions::Contributions(QWidget *parent) : QWidget(parent) {
@@ -64,10 +65,8 @@ void Contributions::searchData() {
 }
 
 void Contributions::showOverview() {
+	QSettings settings;
 	QSqlQuery query(db);
-	query.prepare("SELECT SUM(amount/2) AS amount,for_section FROM pps_invoice WHERE paid_date>=:start AND paid_date<=:end GROUP BY for_section;");
-	query.bindValue(0, dateFrom->date().toString("yyyy-MM-dd"));
-	query.bindValue(1, dateUntil->date().toString("yyyy-MM-dd"));
 	
 	// Clean the GridContainer
 	QLayoutItem *child;
@@ -76,53 +75,86 @@ void Contributions::showOverview() {
 		delete child;
 	}
 	
+	// Load "do not contribute this sections" list
+	QList<QString> dontContribute = settings.value("contributions/dontpay", "members").toStringList();
+	
 	// Refill with Data
+	query.prepare("SELECT SUM(amount/2) AS amount,for_section FROM pps_invoice WHERE paid_date>=:start AND paid_date<=:end GROUP BY for_section;");
+	query.bindValue(0, dateFrom->date().toString("yyyy-MM-dd"));
+	query.bindValue(1, dateUntil->date().toString("yyyy-MM-dd"));
 	query.exec();
+	double sum = 0.0;
 	int row = 0;
 	while (query.next()) {
-		QLabel *s = new QLabel( tr("Section: %1").arg(query.value(1).toString()) );
-		QLabel *a = new QLabel( tr("%1 sFr.").arg(query.value(0).toDouble(), 0, 'f', 2) );
-		a->setAlignment(Qt::AlignRight);
-		overviewLayout->addWidget(s, row, 0);
-		overviewLayout->addWidget(a, row, 1);
-		row++;
+		if (!dontContribute.contains(query.value(1).toString())) {
+			QLabel *s = new QLabel( tr("Section: %1").arg(query.value(1).toString()) );
+			QLabel *a = new QLabel( tr("%1 sFr.").arg(query.value(0).toDouble(), 0, 'f', 2) );
+			a->setAlignment(Qt::AlignRight);
+			overviewLayout->addWidget(s, row, 0);
+			overviewLayout->addWidget(a, row, 1);
+			
+			sum += query.value(0).toDouble();
+			row++;
+		}
 	}
+	
+	// Total
+	QLabel *s = new QLabel( tr("<b>Total:</b>") );
+	QLabel *a = new QLabel( tr("<b>%1 sFr.</b>").arg(sum, 0, 'f', 2) );
+	a->setAlignment(Qt::AlignRight);
+	overviewLayout->addWidget(s, row, 0);
+	overviewLayout->addWidget(a, row, 1);
 }
 
 void Contributions::createQif() {
 	QSettings settings;
 	QString valuta = QDate::currentDate().toString("yyyy-MM-dd");
+	QString date = QDate::currentDate().toString("yyyyMMdd");
+	QString memo = settings.value("qif/payee_label", "Contribution: ").toString();
 	QString section, amount;
 	QSqlQuery query2(db);
 	QSqlQuery query(db);
+	
+	// Load "do not contribute this sections" list
+	QList<QString> dontContribute = settings.value("contributions/dontpay", "members").toStringList();
+	
+	// Initialize the QIF and the list with all Section-QIFs
+	sectionQif.clear();
+	QString qif_national("!Type:" + settings.value("qif/account_liability", "Oth L").toString());
+	if (!memoText->text().isEmpty()) {
+		memo = memoText->text();
+	}
+	
+	// Get all Data
 	query.prepare("SELECT SUM(amount/2) AS amount,for_section FROM pps_invoice WHERE paid_date>=:start AND paid_date<=:end GROUP BY for_section;");
 	query.bindValue(0, dateFrom->date().toString("yyyy-MM-dd"));
 	query.bindValue(1, dateUntil->date().toString("yyyy-MM-dd"));
 	query.exec();
-	
-	sectionQif.clear();
-	QString qif_national("!Type:" + settings.value("qif/account_liability", "Oth L").toString());
-	
 	while (query.next()) {
-		section = query.value(0).toString();
 		amount = query.value(0).toString();
+		section = query.value(1).toString();
+		
+		// Don't generate a QIF if the section is not getting paid...
+		if (dontContribute.contains(section)) {
+			continue;
+		}
 		
 		// National Liability QIF
 		qif_national.append("\nD" + valuta);
 		qif_national.append("\nT" + amount);
-		qif_national.append("\nP"+ settings.value("qif/payee_label", "Contribution: ").toString() + section);
-		qif_national.append("\nN");
-		qif_national.append("\nM" + tr("Contribution for ") + section);
-		qif_national.append("\nL" + settings.value("qif/account_contribution", "Section ").toString() + section);
+		qif_national.append("\nP"+ settings.value("qif/contribution_payer", "Pirateparty Switzerland").toString().arg(section));
+		qif_national.append("\nNCONT-" + date + "-" + section.toUpper());
+		qif_national.append("\nM" + memo.arg(section));
+		qif_national.append("\nL" + settings.value("qif/account_contribution", "Contribution_%1").toString().arg(section));
 		qif_national.append("\n^\n");
 		
 		// Section Income QIF
 		QString qif_section("!Type:" + settings.value("qif/account_bank", "Bank").toString());
 		qif_section.append("\nD" + valuta);
 		qif_section.append("\nT" + amount);
-		qif_section.append("\nP"+ settings.value("qif/payee_label", "Contribution: ").toString() + section);
-		qif_section.append("\nN");
-		qif_section.append("\nM" + tr("Contribution for ") + section);
+		qif_section.append("\nP"+ settings.value("qif/contribution_payer", "Pirateparty Switzerland").toString().arg(section));
+		qif_section.append("\nNCONT-" + date + "-" + section.toUpper());
+		qif_section.append("\nM" + memo.arg(section));
 		qif_section.append("\nL" + settings.value("qif/account_income", "Membership Fee").toString());
 		qif_section.append("\n^\n");
 		
@@ -194,6 +226,7 @@ void Contributions::sendEmail() {
 			out.flush();
 			f.close();
 		}
+		mail.clearAttachments();
 		mail.attach(fileName, attachment);
 		
 		// Load the treasurer of the current Section
@@ -201,7 +234,7 @@ void Contributions::sendEmail() {
 		pers.load(sec.treasurer());
 		
 		// TODO: Load Subject and Texts
-		subject = "Contributions...";
+		subject = QString("Contributions for %1").arg(*section);
 		message = "Attatched the Contributions:\n...blubberl blah\n\nGreetings your Treasurer";
 		
 		// Set the Mailtext and send the mail
@@ -210,6 +243,7 @@ void Contributions::sendEmail() {
 		if (pers.email().length() > 0) {
 			email = pers.email().at(0);
 		}
+		qDebug() << "Sending contributions to: " << email;
 		// TODO: Remove if working
 		email = "postmaster@ranta.ch";
 		
