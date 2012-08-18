933a9837e19b98379794de6ed10c623cfa4f2d00
LukyLuke <lukas@delightsoftware.com>
2012-08-18 19:35:11 +0200

    Budget-View-Widget added

---
+++ src/BudgetView.cpp
@@ -0,0 +1,60 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2012  Lukas Zurschmiede <l.zurschmiede@delightsoftware.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "BudgetView.h"
+
+#include <QDebug>
+
+BudgetView::BudgetView(QWidget *parent) : QWidget(parent) {
+	setupUi(this);
+	
+	connect(actionCreateFolder, SIGNAL(triggered()), this, SLOT(createFolder()));
+	connect(actionRemoveFolder, SIGNAL(triggered()), this, SLOT(removeFolder()));
+	connect(actionChangeFolder, SIGNAL(triggered()), this, SLOT(editFolder()));
+	
+	connect(actionCreateEntry, SIGNAL(triggered()), this, SLOT(createEntry()));
+	connect(actionRemoveEntry, SIGNAL(triggered()), this, SLOT(removeEntry()));
+	connect(actionChangeEntry, SIGNAL(triggered()), this, SLOT(editEntry()));
+	
+}
+
+BudgetView::~BudgetView() { }
+
+void BudgetView::createFolder() {
+	qDebug() << "Create Folder...";
+}
+
+void BudgetView::removeFolder() {
+	qDebug() << "Remove Folder...";
+}
+
+void BudgetView::editFolder() {
+	qDebug() << "Edit Folder...";
+}
+
+void BudgetView::createEntry() {
+	qDebug() << "Create Entry...";
+}
+
+void BudgetView::removeEntry() {
+	qDebug() << "Remove Entry...";
+}
+
+void BudgetView::editEntry() {
+	qDebug() << "Edit Entry...";
+}
---
+++ src/BudgetView.h
@@ -0,0 +1,50 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2012  Lukas Zurschmiede <l.zurschmiede@delightsoftware.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BudgetView_H
+#define BudgetView_H
+
+#include "../ui_budget.h"
+
+#include<QWidget>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QString>
+
+class BudgetView : public QWidget, private Ui::BudgetForm {
+Q_OBJECT
+
+private:
+	QSqlDatabase db;
+
+public:
+	BudgetView(QWidget *parent = 0);
+	virtual ~BudgetView();
+
+public slots:
+	void createFolder();
+	void removeFolder();
+	void editFolder();
+	void createEntry();
+	void removeEntry();
+	void editEntry();
+	
+};
+
+#endif // BudgetView_H
